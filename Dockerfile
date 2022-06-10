75d80abce29dd9e1312efa9c610e9f39767d2487
Kevin Contreras <kvncont@gmail.com>
2022-06-10 00:48:54 -0600

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM mcr.microsoft.com/openjdk/jdk:11-ubuntu AS build
+
+WORKDIR /workspace/app
+
+COPY mvnw .
+COPY .mvn .mvn
+COPY pom.xml .
+COPY src src
+
+RUN ./mvnw package
+RUN mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
+
+
+FROM mcr.microsoft.com/openjdk/jdk:11-ubuntu
+
+VOLUME /tmp
+
+ARG DEPENDENCY=/workspace/app/target/dependency
+
+COPY --from=build ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY --from=build ${DEPENDENCY}/META-INF /app/META-INF
+COPY --from=build ${DEPENDENCY}/BOOT-INF/classes /app
+
+ENTRYPOINT ["java","-cp","app:app/lib/*","com.github.kvncont.k8s.app.Application"]
---
+++ src/main/java/com/github/kvncont/k8s/app/PropertiesController.java
@@ -0,0 +1,35 @@
+package com.github.kvncont.k8s.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PropertiesController {
+    
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.user}")
+    private String dbUser;
+
+    @Value("${db.pass}")
+    private String dbPass;
+
+    @RequestMapping(value="/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> index() {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("dbURL", dbUrl);
+        map.put("dbUser", dbUser);
+        map.put("dbPass", dbPass);
+		
+        return new ResponseEntity<>(map, HttpStatus.OK);
+	}
+}
