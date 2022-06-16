7dbf357c54c925a432ebf76ac6957ec9315e4fed
Kevin Contreras <kvncont@gmail.com>
2022-06-16 03:42:59 -0600

    Fix sonarqube qualitygate

--- src/main/java/com/github/kvncont/k8s/app/PropertiesController.java
+++ src/main/java/com/github/kvncont/k8s/app/PropertiesController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +23,7 @@ public class PropertiesController {
     @Value("${db.pass}")
     private String dbPass;
 
-    @RequestMapping(value="/", produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value="/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> index() {
 
         Map<String, String> map = new HashMap<>();
