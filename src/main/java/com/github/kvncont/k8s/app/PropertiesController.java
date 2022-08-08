6556d7266c87128349798a15c18d0e16aaa948d3
Kevin Contreras <kvncont@gmail.com>
2022-08-08 01:53:26 -0600

    feat: improve k8s deployment (aks, agic, kv)

--- src/main/java/com/github/kvncont/k8s/app/PropertiesController.java
+++ src/main/java/com/github/kvncont/k8s/app/PropertiesController.java
@@ -7,8 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,7 +22,7 @@ public class PropertiesController {
     @Value("${db.pass}")
     private String dbPass;
 
-    @RequestMapping(value="/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> index() {
 
         Map<String, String> map = new HashMap<>();
